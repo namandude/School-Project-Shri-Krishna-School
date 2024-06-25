823c81b613641da5b62eabe110db38b35832a42b
Naman Parashar <namandaksh2005@gmail.com>
2024-06-25 18:02:20 +0530

    front end deployement

--- mern-client/src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = function(app) {
-  app.use('/all-notice', createProxyMiddleware({ target: 'http://school-project-shri-krishna-school.onrender.com', changeOrigin: true }));
-  app.use('/all-event', createProxyMiddleware({ target: 'http://school-project-shri-krishna-school.onrender.com', changeOrigin: true }));
-  app.use('/all-news', createProxyMiddleware({ target: 'http://school-project-shri-krishna-school.onrender.com', changeOrigin: true }));
-  app.use('/all-blog', createProxyMiddleware({ target: 'http://school-project-shri-krishna-school.onrender.com', changeOrigin: true }));
-};
