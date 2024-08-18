bacdeaf0bba3398edff0dd714317d77776abcdb4
Naman Parashar <namandaksh2005@gmail.com>
2024-08-18 13:03:31 +0530

    gallery

--- mern-client/src/home/Gallery.jsx
+++ mern-client/src/home/Gallery.jsx
@@ -1,5 +1,41 @@
 
 
+// import React from 'react';
+
+// const Gallery = () => {
+//   return (
+//     <section className="gallery" id="gallery">
+//       <h2 className="text-5xl font-bold text-center mb-10 leading-snug">Gallery</h2>
+//       <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+//         {[     { src: "https://scontent.fbho1-2.fna.fbcdn.net/v/t39.30808-6/413891981_878487863728385_8778670364012454314_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=833d8c&_nc_ohc=C1Tj0UK_JhkQ7kNvgEiFO4f&_nc_ht=scontent.fbho1-2.fna&oh=00_AYAVoE9BDtaKLzDWburhTSPTrAieTGNEnr1Qcdy4x62vsw&oe=66801D86", title: "Campus" },
+//          { src: "https://scontent.fbho1-1.fna.fbcdn.net/v/t39.30808-6/358457380_784796453097527_961723855018156113_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=127cfc&_nc_ohc=hAZdfGne2pUQ7kNvgHnV4jb&_nc_ht=scontent.fbho1-1.fna&oh=00_AYBLKAlXZnmEoNMoUa3ddTU-rdwKQhpmCRzBlxbvi2ZFhw&oe=66801D8C", title: "Moral Values" },
+//           { src: "https://scontent.fbho1-4.fna.fbcdn.net/v/t39.30808-6/355495326_777174190526420_6081781528256518418_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=833d8c&_nc_ohc=2cM63l3TgKQQ7kNvgETs1Ub&_nc_ht=scontent.fbho1-4.fna&oh=00_AYDKc_Qo7h9Qjg5p6qI0EwEtxXwFok8_kWnSJfdjKt1cKg&oe=668045E2", title: "Sports" },
+//           { src: "https://scontent.fbho1-1.fna.fbcdn.net/v/t1.6435-9/117096614_3025665420884591_5446939912434239664_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=13d280&_nc_ohc=AIJdkVZQFmUQ7kNvgFd82Af&_nc_ht=scontent.fbho1-1.fna&oh=00_AYB3MTiH5Ow8mxSdaPxUd_UNBoKP7HIE5zQBlnPzX5GnVg&oe=66A1DDCB", title: "Our Keen Learners" },
+//           { src: "https://scontent.fbho1-2.fna.fbcdn.net/v/t39.30808-6/413932585_878480803729091_3526365065535792953_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=833d8c&_nc_ohc=9sturzi5ifMQ7kNvgE49Ol4&_nc_ht=scontent.fbho1-2.fna&oh=00_AYBCIIVkjSjOFTHNZ0dpafTjw-8XLHRaWKghUikfGgArSg&oe=66804137", title: "Competitions" },
+//           { src: "https://scontent.fbho1-1.fna.fbcdn.net/v/t39.30808-6/345603530_772806894372242_3661060626763919312_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=833d8c&_nc_ohc=QQNZRaDqQ5IQ7kNvgFbt7vK&_nc_ht=scontent.fbho1-1.fna&oh=00_AYClYmwtaEIFo7UX6--joLDu2-Jh5NLLR1EHCM1hUL1BYg&oe=668026A2", title: "Events " },
+//           { src: "https://scontent.fbho1-2.fna.fbcdn.net/v/t39.30808-6/345453416_562053976068286_2046645543222711216_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_ohc=AlF6ddkbA9cQ7kNvgEgclli&_nc_ht=scontent.fbho1-2.fna&oh=00_AYBHjXHctPEOAiPRDYsOgKsWLmkz70T4ItFmIn2YkeKrrg&oe=66804A54", title: "Bright Talent" },
+//           { src: "https://scontent.fbho1-1.fna.fbcdn.net/v/t39.30808-6/432627297_923248789252292_4327597992928091800_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=833d8c&_nc_ohc=Vy0srVsQTHwQ7kNvgG513SW&_nc_ht=scontent.fbho1-1.fna&oh=00_AYBXOa4RFqe4o-BJphH4dBVmDyNdO9WX4sEN_j2VEbhIhw&oe=668030FF", title: "Exhibitions " },
+//           { src: "https://scontent.fbho1-1.fna.fbcdn.net/v/t39.30808-6/414045571_878475860396252_3986578262855933653_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=j6x-1otyOGEQ7kNvgGbj_h0&_nc_ht=scontent.fbho1-1.fna&oh=00_AYCyiJuGFPHOc7mhK2zEbAIlcIFGtkXXiLMD7vH0YLGNNw&oe=66804976", title: "Celebrating the moments" },
+//           { src: "https://scontent.fbho1-2.fna.fbcdn.net/v/t39.30808-6/413891940_878466890397149_9149827048249554723_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=833d8c&_nc_ohc=Bx0Zswz1rUYQ7kNvgG0BRJV&_nc_ht=scontent.fbho1-2.fna&oh=00_AYDkaMgIo6i-HWe1Uj3ShjGeOPf93yQPDiNnAmGZk-RMIQ&oe=668033A3", title: "Educational Tours" },
+         
+     
+//         ].map((item, index) => (
+//           <div key={index} className="relative w-full h-60 overflow-hidden rounded-lg cursor-pointer group">
+//             <img src={item.src} alt={item.title} className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110" />
+//             <h3 className="absolute inset-0 bg-teal-600 bg-opacity-75 text-white text-center flex items-center justify-center text-xl opacity-0 transition-opacity duration-500 group-hover:opacity-100">
+//               {item.title}
+//             </h3>
+//           </div>
+//         ))}
+//       </div>
+//     </section>
+//   );
+// }
+
+// export default Gallery;
+
+
+
 import React from 'react';
 
 const Gallery = () => {
@@ -7,18 +43,17 @@ const Gallery = () => {
     <section className="gallery" id="gallery">
       <h2 className="text-5xl font-bold text-center mb-10 leading-snug">Gallery</h2>
       <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-        {[     { src: "https://scontent.fbho1-2.fna.fbcdn.net/v/t39.30808-6/413891981_878487863728385_8778670364012454314_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=833d8c&_nc_ohc=C1Tj0UK_JhkQ7kNvgEiFO4f&_nc_ht=scontent.fbho1-2.fna&oh=00_AYAVoE9BDtaKLzDWburhTSPTrAieTGNEnr1Qcdy4x62vsw&oe=66801D86", title: "Campus" },
-         { src: "https://scontent.fbho1-1.fna.fbcdn.net/v/t39.30808-6/358457380_784796453097527_961723855018156113_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=127cfc&_nc_ohc=hAZdfGne2pUQ7kNvgHnV4jb&_nc_ht=scontent.fbho1-1.fna&oh=00_AYBLKAlXZnmEoNMoUa3ddTU-rdwKQhpmCRzBlxbvi2ZFhw&oe=66801D8C", title: "Moral Values" },
-          { src: "https://scontent.fbho1-4.fna.fbcdn.net/v/t39.30808-6/355495326_777174190526420_6081781528256518418_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=833d8c&_nc_ohc=2cM63l3TgKQQ7kNvgETs1Ub&_nc_ht=scontent.fbho1-4.fna&oh=00_AYDKc_Qo7h9Qjg5p6qI0EwEtxXwFok8_kWnSJfdjKt1cKg&oe=668045E2", title: "Sports" },
-          { src: "https://scontent.fbho1-1.fna.fbcdn.net/v/t1.6435-9/117096614_3025665420884591_5446939912434239664_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=13d280&_nc_ohc=AIJdkVZQFmUQ7kNvgFd82Af&_nc_ht=scontent.fbho1-1.fna&oh=00_AYB3MTiH5Ow8mxSdaPxUd_UNBoKP7HIE5zQBlnPzX5GnVg&oe=66A1DDCB", title: "Our Keen Learners" },
-          { src: "https://scontent.fbho1-2.fna.fbcdn.net/v/t39.30808-6/413932585_878480803729091_3526365065535792953_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=833d8c&_nc_ohc=9sturzi5ifMQ7kNvgE49Ol4&_nc_ht=scontent.fbho1-2.fna&oh=00_AYBCIIVkjSjOFTHNZ0dpafTjw-8XLHRaWKghUikfGgArSg&oe=66804137", title: "Competitions" },
-          { src: "https://scontent.fbho1-1.fna.fbcdn.net/v/t39.30808-6/345603530_772806894372242_3661060626763919312_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=833d8c&_nc_ohc=QQNZRaDqQ5IQ7kNvgFbt7vK&_nc_ht=scontent.fbho1-1.fna&oh=00_AYClYmwtaEIFo7UX6--joLDu2-Jh5NLLR1EHCM1hUL1BYg&oe=668026A2", title: "Events " },
-          { src: "https://scontent.fbho1-2.fna.fbcdn.net/v/t39.30808-6/345453416_562053976068286_2046645543222711216_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_ohc=AlF6ddkbA9cQ7kNvgEgclli&_nc_ht=scontent.fbho1-2.fna&oh=00_AYBHjXHctPEOAiPRDYsOgKsWLmkz70T4ItFmIn2YkeKrrg&oe=66804A54", title: "Bright Talent" },
-          { src: "https://scontent.fbho1-1.fna.fbcdn.net/v/t39.30808-6/432627297_923248789252292_4327597992928091800_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=833d8c&_nc_ohc=Vy0srVsQTHwQ7kNvgG513SW&_nc_ht=scontent.fbho1-1.fna&oh=00_AYBXOa4RFqe4o-BJphH4dBVmDyNdO9WX4sEN_j2VEbhIhw&oe=668030FF", title: "Exhibitions " },
-          { src: "https://scontent.fbho1-1.fna.fbcdn.net/v/t39.30808-6/414045571_878475860396252_3986578262855933653_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=j6x-1otyOGEQ7kNvgGbj_h0&_nc_ht=scontent.fbho1-1.fna&oh=00_AYCyiJuGFPHOc7mhK2zEbAIlcIFGtkXXiLMD7vH0YLGNNw&oe=66804976", title: "Celebrating the moments" },
-          { src: "https://scontent.fbho1-2.fna.fbcdn.net/v/t39.30808-6/413891940_878466890397149_9149827048249554723_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=833d8c&_nc_ohc=Bx0Zswz1rUYQ7kNvgG0BRJV&_nc_ht=scontent.fbho1-2.fna&oh=00_AYDkaMgIo6i-HWe1Uj3ShjGeOPf93yQPDiNnAmGZk-RMIQ&oe=668033A3", title: "Educational Tours" },
-         
-     
+        {[
+          { src: "/camous.jpg", title: "Campus" },
+          { src: "/kidos.jpg", title: "Moral Values" },
+          { src: "/sports.jpg", title: "Sports" },
+          { src: "/celebs.jpg", title: "Our Keen Learners" },
+          { src: "/yoga.jpg", title: "Competitions" },
+          { src: "/grand.jpg", title: "Events" },
+          { src: "/ed.jpg", title: "Bright Talent" },
+          { src: "/exhibition.jpg", title: "Exhibitions" },
+          { src: "/edugrad.jpg", title: "Celebrating the moments" },
+          { src: "/edutour.jpg", title: "Educational Tours" },
         ].map((item, index) => (
           <div key={index} className="relative w-full h-60 overflow-hidden rounded-lg cursor-pointer group">
             <img src={item.src} alt={item.title} className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110" />
@@ -33,5 +68,3 @@ const Gallery = () => {
 }
 
 export default Gallery;
-
-
--- mern-client/src/home/Reviews.jsx
+++ mern-client/src/home/Reviews.jsx
@@ -58,7 +58,7 @@ const Reviews = () => {
                
 
                 <div className='mt-5'>
-                    <img src="https://scontent.fbho1-4.fna.fbcdn.net/v/t39.30808-6/434149139_933070834936754_8163954356445880134_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=833d8c&_nc_ohc=nsajWS5xnc0Q7kNvgGBDpJN&_nc_ht=scontent.fbho1-4.fna&oh=00_AYB4XTnpb69NNPOExxaVbPYRUCPH-_BsXngmcK6xVaizFQ&oe=668015BA" alt="" />
+                    <img src="/toopers.jpg" alt="" />
                 <h3 className='text-lg font-medium'>Academic</h3>
                 <p className='text-base'>Rank Holders</p>
                 </div>
@@ -69,7 +69,7 @@ const Reviews = () => {
                
 
                 <div className='mt-5'>
-                    <img src="https://scontent.fbho1-3.fna.fbcdn.net/v/t39.30808-6/432758385_923248829252288_7965401679146061214_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=833d8c&_nc_ohc=27l-t9qNRMoQ7kNvgEMXvLb&_nc_ht=scontent.fbho1-3.fna&oh=00_AYBI3PsBjdls1QenOHexyPCrH7ob-NszqvQZWoH3msLepg&oe=6680458C" alt="" />
+                    <img src="/acid.jpg" alt="" />
                 <h3 className='text-lg font-medium'>Exhibitions</h3>
                 <p className='text-base'>Science Tech Exploration</p>
                 </div>
@@ -80,7 +80,7 @@ const Reviews = () => {
                
 
                 <div className='mt-5'>
-                    <img src="https://scontent.fbho1-3.fna.fbcdn.net/v/t39.30808-6/431283188_923249672585537_470305251247149954_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=833d8c&_nc_ohc=g_ZgxjfKxyUQ7kNvgFH_djj&_nc_ht=scontent.fbho1-3.fna&oh=00_AYCuAd1Eb4ndk-HEutNmpwMlK7eEW7sRG6iEiaSNo_61Ag&oe=6680309B" alt="" />
+                    <img src="/water.jpg" alt="" />
                 <h3 className='text-lg font-medium'>Science and Tech</h3>
                 <p className='text-base'>Exploration in Space</p>
                 </div>
@@ -91,7 +91,7 @@ const Reviews = () => {
                
 
                 <div className='mt-5'>
-                    <img src="https://scontent.fbho1-4.fna.fbcdn.net/v/t39.30808-6/387874627_839282850982220_245580338748213734_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=833d8c&_nc_ohc=apQ53AjKLrkQ7kNvgHAWp0i&_nc_ht=scontent.fbho1-4.fna&oh=00_AYDu1riqJUgG99ks5bKyB_FIhl-yrpsVoxggwWHCJ6qrlw&oe=6680401B" alt="" />
+                    <img src="/boy.jpg" alt="Education" />
                 <h3 className='text-lg font-medium'>Education Achievement </h3>
                 <p className='text-base'>School Rank Holder</p>
                 </div>
@@ -102,7 +102,7 @@ const Reviews = () => {
                
 
                 <div className='mt-5'>
-                    <img src="https://scontent.fbho1-1.fna.fbcdn.net/v/t39.30808-6/413889314_878462910397547_3469208609001128791_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=MI_xYPygYnIQ7kNvgFPWL9f&_nc_ht=scontent.fbho1-1.fna&oh=00_AYAm2txF9pLmepul6Xbg1MojujnK3QHAnzx7CPPMITtlLg&oe=668024D6" alt="" />
+                    <img src="/gold.jpg" alt="" />
                 <h3 className='text-lg font-medium'>Trips and Learning</h3>
                 <p className='text-base'>Learn with fun!</p>
                 </div>
@@ -113,7 +113,7 @@ const Reviews = () => {
                
 
                 <div className='mt-5'>
-                    <img src="https://scontent.fbho1-4.fna.fbcdn.net/v/t39.30808-6/345430613_1428585491285231_2930947670283393225_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=833d8c&_nc_ohc=fabyx2QeP_0Q7kNvgGGtOnN&_nc_ht=scontent.fbho1-4.fna&oh=00_AYD7WlKmBOGONY2Zfj5P_eYLsmCPFmp820Dmsavh0QoGgg&oe=6680269E" alt="" />
+                    <img src="/yoga.jpg" alt="" />
                 <h3 className='text-lg font-medium'>Talents</h3>
                 <p className='text-base'>Showcasing the best in them. </p>
                 </div>
@@ -130,3 +130,68 @@ const Reviews = () => {
 
 export default Reviews
 
+
+
+// import React from 'react';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import { FaStar } from "react-icons/fa6";
+// import 'swiper/css';
+// import 'swiper/css/pagination';
+// import { Pagination, Autoplay } from 'swiper/modules';
+
+// const Reviews = () => {
+//   return (
+//     <div className='my-12 px-4 lg:px-24'>
+//       <h2 className='text-5xl font-bold text-center mb-10 leading-snug ml-0 sm:ml-20'>Achievements</h2>
+//       <p>Our achievers demonstrate that success in the coeval world relies not only on cognizance, but also on imagination, entrepreneurship, and the ability to explore.</p>
+//       <div>
+//         <Swiper
+//           slidesPerView={1}
+//           spaceBetween={30}
+//           autoplay={{ delay: 1000 }}
+//           loop={true}
+//           pagination={{ clickable: true }}
+//           breakpoints={{
+//             640: { slidesPerView: 1, spaceBetween: 20 },
+//             768: { slidesPerView: 2, spaceBetween: 40 },
+//             1024: { slidesPerView: 3, spaceBetween: 50 },
+//           }}
+//           modules={[Pagination, Autoplay]}
+//           className="mySwiper"
+//         >
+//           {/* Slides */}
+//           <SwiperSlide className='shadow-2xl bg-transparent py-8 px-4 md:m-5 rounded-lg border'>
+//             <div className='space=y-6'>
+//               <div className='mt-5'>
+//                 <img src="/toopers.jpg" alt="Academic" />
+//                 <h3 className='text-lg font-medium'>Academic</h3>
+//                 <p className='text-base'>Rank Holders</p>
+//               </div>
+//             </div>
+//           </SwiperSlide>
+//           <SwiperSlide className='shadow-2xl bg-white py-8 px-4 md:m-5 rounded-lg border'>
+//             <div className='space=y-6'>
+//               <div className='mt-5'>
+//                 <img src="/exhibition.jpg" alt="Exhibitions" />
+//                 <h3 className='text-lg font-medium'>Exhibitions</h3>
+//                 <p className='text-base'>Science Tech Exploration</p>
+//               </div>
+//             </div>
+//           </SwiperSlide>
+//           <SwiperSlide className='shadow-2xl bg-white py-8 px-4 md:m-5 rounded-lg border'>
+//             <div className='space=y-6'>
+//               <div className='mt-5'>
+//                 <img src="/ed.jpg" alt="Science and Tech" />
+//                 <h3 className='text-lg font-medium'>Science and Tech</h3>
+//                 <p className='text-base'>Exploration in Space</p>
+//               </div>
+//             </div>
+//           </SwiperSlide>
+//           {/* Add more SwiperSlide components as needed */}
+//         </Swiper>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Reviews;
